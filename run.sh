ef650fc4d3b1d994761c533dba5971672f677a55
Maxime Vidori <maxime.vidori@gmail.com>
2015-03-28 17:24:23 +0100

    fix errors in the run.sh script, refactor names

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-CONTAINER_NAME="html5-boilerplate"
+IMAGE_NAME="html5-boilerplate"
 
 # Echo the command prefixed with '++' than run it
 echo_and_run () {
@@ -10,21 +10,21 @@ echo_and_run () {
 }
 
 # Check if the image exists in docker, otherwise run it
-if ! docker images | grep -q "$CONTAINER_NAME"
+if ! docker images | grep -q "$IMAGE_NAME"
 then
     echo_and_run \
-    docker build -t "$CONTAINER_NAME" github.com/IxDay/docker-html5-boilerplate
+    docker build -t "$IMAGE_NAME" github.com/IxDay/docker-html5-boilerplate
 fi
 
 # Initialize the directory if empty
-[ "$(ls -A .)" ] || echo_and_run docker -v $(pwd):/mnt "$CONTAINER_NAME" init
+[ "$(ls -A .)" ] || echo_and_run docker run -v $(pwd):/mnt "$IMAGE_NAME" init
 
 # Checks if a container with the same name is not already running
-HTML_CONTAINER=$(docker ps | awk '$2 ~ /'"$CONTAINER_NAME"'/ { print $1 }')
+CONTAINER=$(docker ps | awk '$2 ~ /'"$IMAGE_NAME"'/ { print $1 }')
 
-if [ ! -z $HTML_CONTAINER ]
+if [ ! -z $CONTAINER ]
 then
-    echo "The container is already running, its id is $HTML_CONTAINER"
+    echo "The container is already running, its id is $CONTAINER"
     while true
     do
         echo "kill it? [y/N]"
@@ -32,9 +32,9 @@ then
         [ -z $KILL ] && exit
         [ "$KILL" = "y" ] && break
     done
-    echo_and_run docker kill $HTML_CONTAINER
+    echo_and_run docker kill $CONTAINER
 fi
 
 # Run the container and serve pwd directly with livereload
-echo_and_run docker -v $(pwd):/mnt html5-boilerplate
+echo_and_run docker run -v $(pwd):/mnt html5-boilerplate
 
